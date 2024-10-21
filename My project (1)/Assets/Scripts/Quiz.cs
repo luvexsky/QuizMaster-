8e8772204ee180fee24e921538023c600bb66b60
luvexsky <ngryemo@gmail.com>
2024-10-20 20:20:17 -0400

    "Implemented answer feedback with sprite swapping."

--- My project (1)/Assets/Scripts/Quiz.cs
+++ My project (1)/Assets/Scripts/Quiz.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+using System;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questiontext;
    [SerializeField] QuestionSO question;
    [SerializeField] GameObject[] answerButtons;
+   int correctanswerindex;
+   [SerializeField] Sprite defaultAnswerSprite;
+   [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questiontext.text = question.GetQuestion();
 
-        TextMeshProUGUI buttonText = answerButtons[0].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text = question.GetAnswer(0);
+for(int i = 0; i < answerButtons.Length; i++)
+{
+        TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = question.GetAnswer(i);
+}
     }
+
+   public void OnAnswerSelected(int index)
+{ 
+    if (index == question.GetCorrectAnswerIndex())
+    { 
+        questiontext.text = "Correct!";
+        Image buttonImage = answerButtons[index].GetComponent<Image>();
+        buttonImage.Sprite = correctAnswerSprite;
+    }
+    
+}
+
+    
 }
